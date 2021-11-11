f26c616409c52f1aeffa6f68247a3c9be4962673
excelsior <excelsior.valentino@ti.ukdw.ac.id>
2021-11-11 21:52:04 +0800

    soal 1

---
+++ 1_E_71210785.py
@@ -0,0 +1,6 @@
+# Program sederhana perhitungan luas trapesium
+aa = int(input("Masukan alas atas : "))
+ab = int(input("Masukan alas bawah : "))
+t = int(input("Masukan tinggi : "))
+L = 0.5 * (aa + ab) * t
+print ("Luas trapesium adalah :" ,L)
