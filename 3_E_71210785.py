43a6eac99c58079690bb19bb5dd3dfcdaff9a390
excelsior <excelsior.valentino@ti.ukdw.ac.id>
2021-11-11 21:52:40 +0800

    soal 3

---
+++ 3_E_71210785.py
@@ -0,0 +1,9 @@
+# fitur menambah catatan di program Dani
+a = input("Masukan daftar belanja 1: ")
+b = input("Masukan daftar belanja 2: ")
+c = input("Tambahkan data ke daftar belanja 1: ")
+d = input("Tambahkan data ke daftar belanja 1: ")
+print ("Daftar belanja 1 adalah" ,a +',', end="")
+print (c)
+print ("Daftar belanja 2 adalah" ,b +',', end="")
+print (d)
