db8a70437aa1b14512095d752f82e898a9adecaa
excelsior <excelsior.valentino@ti.ukdw.ac.id>
2021-11-11 21:55:52 +0800

    soal 4

--- 4_E_71210785.py
+++ 4_E_71210785.py
@@ -1,11 +1,11 @@
-u = int(input("Dari Suku berapa : "))
+u = int(input("Dari Suku berapa : ")) 
 un = int(input("Suku akhir : "))
 a = float(input("Angka awal : "))
 r = float(input("Rasio : "))
 
 hasil = 0
 for n in range(u, un+1):
-    suku = a*(r**(n-1))
+    suku = a*(r**(n-1)) # Rumus mencari suku
     print(suku)
 
 print('Jumlah suku ke',un, end="")
