8de8a6c9834639f42c4907740ecbde307381cb3d
excelsior <excelsior.valentino@ti.ukdw.ac.id>
2021-11-11 22:05:30 +0800

    soal 2

--- 2_E_71210785.py
+++ 2_E_71210785.py
@@ -1,17 +1,8 @@
-n = input("Nama: ")
-l = input("Tempat tanggal lahir : ")
+n = input("Nama : ")
+ttl = input("Tempat tanggal lahir : ")
 
-a = n.split()
-b = l.split()
+a = n.rsplit(" ",1)
+b = ttl.split(" ",1)
 
-print("Haloo!", a[1], ",", a[0])
-print("Anda lahir di", b[0], "pada tanggal", b[1], b[2], b[3])
-
-n = input("Nama: ")
-l = input("Tempat tanggal lahir : ")
-
-a = n.split()
-b = l.split()
-
-print("Haloo!", a[1], ",", a[0], a[1])
-print("Anda lahir di", b[0], "pada tanggal", b[1], b[2], b[3])
+print("Haloo!" ,a[1] +"," ,a[0])
+print("Anda lahir di" ,b[0] ,"pada tanggal" ,b[1])
